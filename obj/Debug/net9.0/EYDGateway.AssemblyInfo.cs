79d669a7420479414a693661e7c8aed277ba1c42
James Kuspisz <jkuspisz@gmail.com>
2025-08-10 22:26:06 +0100

    Ad Hoc ES Report: add models, controller, view; fix Razor issues; remove pending migration

--- obj/Debug/net9.0/EYDGateway.AssemblyInfo.cs
+++ obj/Debug/net9.0/EYDGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EYDGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2adfba1ce6c048d59a193ae402c90752d5b0143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a62aafa5a8e6bce031a8600abada6388fd8c4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EYDGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EYDGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
